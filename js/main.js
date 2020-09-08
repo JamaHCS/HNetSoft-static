8909bc8fdbea579ab8957830ad881fde092b9acb
JamaHCS <jama117@hotmail.com>
2020-09-07 23:31:43 -0500

    [add] Slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-new Splide('.splide').mount();
+const options = {
+  type: 'loop',
+  speed: 1000,
+  easing: 'ease',
+  arrows: false,
+  autoplay: true,
+  interval: 4000,
+  pauseOnHover: false,
+  height: 'calc(100vh - 120px)',
+  cover: true,
+  focus: 'center',
+};
+
+new Splide('.splide', options).mount();
