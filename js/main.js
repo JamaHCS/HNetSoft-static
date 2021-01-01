aa22e3e25b4f18dd8e9f28156637e04e293428ea
JamaHCS <jama117@hotmail.com>
2020-12-31 20:40:15 -0600

    [Retaking]

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //? Slider
 const options = {
   type: 'loop',
-  speed: 1000,
+  speed: 500,
   easing: 'ease',
   arrows: false,
   autoplay: true,
-  interval: 4000,
+  interval: 7000,
   pauseOnHover: false,
   height: 'calc(100vh - 120px)',
   cover: true,
